20ee1e639d0a1ef81f295bf05c4b95cda3361243
ngirimana <chadrackngirimana@gmail.com>
2021-04-07 17:06:33 +0200

    add correct route

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use('/api/v1/', documentRoute);
 if (process.env.NODE_ENV === 'PRODUCTION') {
 	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-	app.get('*', (req, res) => {
+	app.get('/', (req, res) => {
 		res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
 	});
 }
