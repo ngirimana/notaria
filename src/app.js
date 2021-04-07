9fa29964fd3143a43d03937304365e9cd1850aeb
ngirimana <chadrackngirimana@gmail.com>
2021-04-07 17:21:51 +0200

    home route

--- src/app.js
+++ src/app.js
@@ -35,7 +35,8 @@ app.use('/api/v1/', documentRoute);
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
 	app.use(express.static(path.join(__dirname, '../frontend/build')));
-	app.use('/', (req, res) => {
+
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
 	});
 }
